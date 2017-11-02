b90a1343704af66178493bd69833f45e59a37473
Jens Schulze <jens.schulze@commercetools.de>
2017-11-02 11:28:40 +0100

    improve string case formatter to recognise embedded upper case words

--- raml-model/src/main/java/io/vrap/rmf/raml/model/util/StringCaseFormat.java
+++ raml-model/src/main/java/io/vrap/rmf/raml/model/util/StringCaseFormat.java
@@ -1,9 +1,7 @@
 package io.vrap.rmf.raml.model.util;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.CharMatcher;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -16,24 +14,24 @@ import java.util.stream.Stream;
  * in string template transformations.
  */
 public enum StringCaseFormat implements Predicate<String>, Function<String, String> {
-    LOWER_CAMEL_CASE(s -> s.matches("\\p{Lower}(\\p{Lower}|\\p{Upper})*"), CharMatcher.inRange('A', 'Z'), StringCaseFormat::lcFirst, StringCaseFormat::ucFirst),
-    UPPER_CAMEL_CASE(s -> s.matches("\\p{Upper}(\\p{Lower}|\\p{Upper})*"), CharMatcher.inRange('a', 'z'), StringCaseFormat::ucFirst, StringCaseFormat::ucFirst),
+    LOWER_CAMEL_CASE(s -> s.matches("\\p{Lower}(\\p{Lower}|\\p{Upper})*"), s -> Arrays.asList(s.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")), StringCaseFormat::lcFirst, StringCaseFormat::ucFirst),
+    UPPER_CAMEL_CASE(s -> s.matches("\\p{Upper}(\\p{Lower}|\\p{Upper})*"), s -> Arrays.asList(s.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")), StringCaseFormat::ucFirst, StringCaseFormat::ucFirst),
 
-    LOWER_HYPHEN_CASE(s -> s.matches("\\p{Lower}(-|\\p{Lower})+"), null, "-", String::toLowerCase),
-    UPPER_HYPHEN_CASE(s -> s.matches("\\p{Upper}(-|\\p{Upper})+"), null, "-", String::toUpperCase),
+    LOWER_HYPHEN_CASE(s -> s.matches("\\p{Lower}(-|\\p{Lower})+"), s -> Arrays.asList(s.split("-")), "-", String::toLowerCase),
+    UPPER_HYPHEN_CASE(s -> s.matches("\\p{Upper}(-|\\p{Upper})+"), s -> Arrays.asList(s.split("-")), "-", String::toUpperCase),
 
-    LOWER_UNDERSCORE_CASE(s -> s.matches("\\p{Lower}(_|\\p{Lower})+"), null, "_", String::toLowerCase),
-    UPPER_UNDERSCORE_CASE(s -> s.matches("\\p{Upper}(_|\\p{Upper})+"), null, "_", String::toUpperCase)
+    LOWER_UNDERSCORE_CASE(s -> s.matches("\\p{Lower}(_|\\p{Lower})+"), s -> Arrays.asList(s.split("_")), "_", String::toLowerCase),
+    UPPER_UNDERSCORE_CASE(s -> s.matches("\\p{Upper}(_|\\p{Upper})+"), s -> Arrays.asList(s.split("_")), "_", String::toUpperCase)
     ;
 
 
     private final Predicate<String> test;
-    private final CharMatcher wordSeparator;
+    private final Function<String, List<String>> wordSeparator;
     private final String delimiter;
     private final Function<String, String> firstWordTransform;
     private final Function<String, String> otherWordTransform;
 
-    StringCaseFormat(final Predicate<String> test, final CharMatcher wordSeparator, final String delimiter, Function<String, String> firstWordTransform, Function<String, String> otherWordTransform) {
+    StringCaseFormat(final Predicate<String> test, final Function<String, List<String>> wordSeparator, final String delimiter, Function<String, String> firstWordTransform, Function<String, String> otherWordTransform) {
         this.test = test;
         this.wordSeparator = wordSeparator;
         this.delimiter = delimiter;
@@ -42,12 +40,12 @@ public enum StringCaseFormat implements Predicate<String>, Function<String, Stri
     }
 
 
-    StringCaseFormat(final Predicate<String> test, final CharMatcher wordSeparator, final String delimiter, Function<String, String> wordTransform) {
+    StringCaseFormat(final Predicate<String> test, final Function<String, List<String>> wordSeparator, final String delimiter, Function<String, String> wordTransform) {
         this(test, wordSeparator, delimiter, wordTransform, wordTransform);
     }
 
 
-    StringCaseFormat(final Predicate<String> test, final CharMatcher wordSeparator, Function<String, String> firstWordTransform, Function<String, String> otherWordTransform) {
+    StringCaseFormat(final Predicate<String> test, final Function<String, List<String>> wordSeparator, Function<String, String> firstWordTransform, Function<String, String> otherWordTransform) {
         this(test, wordSeparator, "", firstWordTransform, otherWordTransform);
     }
 
@@ -93,24 +91,7 @@ public enum StringCaseFormat implements Predicate<String>, Function<String, Stri
 
     @VisibleForTesting
     List<String> compoundWords(final String value) {
-        final List<String> parts;
-        if (delimiter.isEmpty()) {
-            parts = new ArrayList<>();
-            int i = 0;
-            int wordStart = 0;
-            for (; i < value.length(); i++) {
-                if (wordSeparator.matches(value.charAt(i)) && wordStart < i) {
-                    parts.add(value.substring(wordStart, i));
-                    wordStart = i;
-                }
-            }
-            if (wordStart < i) {
-                parts.add(value.substring(wordStart, i));
-            }
-        } else {
-            parts = Arrays.asList(value.split(delimiter));
-        }
-        return parts;
+        return wordSeparator.apply(value);
     }
 
     private final static String lcFirst(final String value) {
--- raml-model/src/test/groovy/io/vrap/rmf/raml/model/util/StringCaseFormatTest.groovy
+++ raml-model/src/test/groovy/io/vrap/rmf/raml/model/util/StringCaseFormatTest.groovy
@@ -13,6 +13,8 @@ class StringCaseFormatTest extends Specification {
         where:
         format                                 | value   || result
         StringCaseFormat.LOWER_CAMEL_CASE      | 'aBcD'  || true
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'aBCDEf'  || true
+        StringCaseFormat.UPPER_CAMEL_CASE      | 'ABCDef'  || true
         StringCaseFormat.UPPER_CAMEL_CASE      | 'AbcD'  || true
         StringCaseFormat.LOWER_HYPHEN_CASE     | 'ab-cd' || true
         StringCaseFormat.LOWER_HYPHEN_CASE     | '-cd'   || false
@@ -24,11 +26,13 @@ class StringCaseFormatTest extends Specification {
         format.compoundWords(value) == result
         where:
         format                                 | value   || result
-        StringCaseFormat.LOWER_CAMEL_CASE      | ''      || []
-        StringCaseFormat.LOWER_CAMEL_CASE      | 'ABCD'  || ['A', 'B', 'C', 'D']
+        StringCaseFormat.LOWER_CAMEL_CASE      | ''      || ['']
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'ABCD'  || ['ABCD']
         StringCaseFormat.LOWER_CAMEL_CASE      | 'aBcD'  || ['a', 'Bc', 'D']
         StringCaseFormat.LOWER_CAMEL_CASE      | 'aBcd'  || ['a', 'Bcd']
         StringCaseFormat.LOWER_CAMEL_CASE      | 'abcd'  || ['abcd']
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'aBCDEf' || ['a', 'BCD', 'Ef']
+        StringCaseFormat.UPPER_CAMEL_CASE      | 'ABCDef'  || ['ABC', 'Def']
         StringCaseFormat.LOWER_HYPHEN_CASE     | 'ab-cd' || ['ab', 'cd']
         StringCaseFormat.LOWER_UNDERSCORE_CASE | 'ab_cd' || ['ab', 'cd']
     }
@@ -38,23 +42,36 @@ class StringCaseFormatTest extends Specification {
         format.render(compoundWords) == result
         where:
         format                                 | compoundWords || result
-        StringCaseFormat.LOWER_CAMEL_CASE      | ['Ab', "Bd"]  || 'abBd'
-        StringCaseFormat.UPPER_CAMEL_CASE      | ['ab', "Bd"]  || 'AbBd'
-        StringCaseFormat.LOWER_UNDERSCORE_CASE | ['Ab', "Bd"]  || 'ab_bd'
-        StringCaseFormat.UPPER_UNDERSCORE_CASE | ['ab', "Bd"]  || 'AB_BD'
-        StringCaseFormat.LOWER_HYPHEN_CASE     | ['Ab', "Bd"]  || 'ab-bd'
-        StringCaseFormat.UPPER_HYPHEN_CASE     | ['ab', "Bd"]  || 'AB-BD'
+        StringCaseFormat.LOWER_CAMEL_CASE      | ['Ab', 'Bd']  || 'abBd'
+        StringCaseFormat.UPPER_CAMEL_CASE      | ['ab', 'Bd']  || 'AbBd'
+        StringCaseFormat.LOWER_UNDERSCORE_CASE | ['Ab', 'Bd']  || 'ab_bd'
+        StringCaseFormat.UPPER_UNDERSCORE_CASE | ['ab', 'Bd']  || 'AB_BD'
+        StringCaseFormat.LOWER_UNDERSCORE_CASE | ['']          || ''
+        StringCaseFormat.LOWER_HYPHEN_CASE     | ['']          || ''
+        StringCaseFormat.LOWER_HYPHEN_CASE     | ['Ab', 'Bd']  || 'ab-bd'
+        StringCaseFormat.UPPER_HYPHEN_CASE     | ['ab', 'Bd']  || 'AB-BD'
+        StringCaseFormat.UPPER_HYPHEN_CASE     | ['']          || ''
+        StringCaseFormat.UPPER_UNDERSCORE_CASE | ['']          || ''
+
     }
 
     def "apply"() {
         expect:
         format.apply(value) == result
         where:
-        format                                 | value   || result
-        StringCaseFormat.LOWER_CAMEL_CASE      | ''      || ''
-        StringCaseFormat.LOWER_CAMEL_CASE      | 'ab-cd' || 'abCd'
-        StringCaseFormat.LOWER_CAMEL_CASE      | 'AB-CD' || 'abCd'
-        StringCaseFormat.UPPER_UNDERSCORE_CASE | 'ab-cd' || 'AB_CD'
-        StringCaseFormat.UPPER_HYPHEN_CASE     | 'ab-cd' || 'AB-CD'
+        format                                 | value    || result
+        StringCaseFormat.LOWER_CAMEL_CASE      | ''       || ''
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'ab-cd'  || 'abCd'
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'AB-CD'  || 'abCd'
+        StringCaseFormat.UPPER_UNDERSCORE_CASE | 'ab-cd'  || 'AB_CD'
+        StringCaseFormat.UPPER_HYPHEN_CASE     | 'ab-cd'  || 'AB-CD'
+        StringCaseFormat.LOWER_HYPHEN_CASE     | 'aBCDEf' || 'a-bcd-ef'
+        StringCaseFormat.LOWER_HYPHEN_CASE     | 'ABCDef' || 'abc-def'
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'ABCDef' || 'abcDef'
+        StringCaseFormat.UPPER_CAMEL_CASE      | 'ABCDef' || 'AbcDef'
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'ABCdEf' || 'abCdEf'
+        StringCaseFormat.UPPER_CAMEL_CASE      | 'ABCdEf' || 'AbCdEf'
+        StringCaseFormat.LOWER_CAMEL_CASE      | 'aBCDEf' || 'aBcdEf'
+        StringCaseFormat.UPPER_CAMEL_CASE      | 'aBCDEf' || 'ABcdEf'
     }
 }
