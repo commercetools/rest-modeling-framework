b958970024c735570e4925ba4ec4f05b5bc7ff4d
katmatt <matthias.koester@gmail.com>
2017-11-12 11:40:04 +0100

    Add validation of enum facet.

--- raml-model/src/main/java/io/vrap/rmf/raml/validation/TypesValidator.java
+++ raml-model/src/main/java/io/vrap/rmf/raml/validation/TypesValidator.java
@@ -1,6 +1,7 @@
 package io.vrap.rmf.raml.validation;
 
 import io.vrap.rmf.raml.model.facets.*;
+import io.vrap.rmf.raml.model.types.AnyAnnotationType;
 import io.vrap.rmf.raml.model.types.AnyType;
 import io.vrap.rmf.raml.model.types.util.TypesSwitch;
 import org.eclipse.emf.common.util.Diagnostic;
@@ -8,13 +9,12 @@ import org.eclipse.emf.common.util.DiagnosticChain;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TypesValidator extends AbstractRamlValidator {
     private final TypesValidatingVisitor typesValidatingVisitor = new TypesValidatingVisitor();
+    private final EnumValidatingVisitor enumValidatingVisitor = new EnumValidatingVisitor();
     private final ExamplesValidatingVisitor examplesValidatingVisitor = new ExamplesValidatingVisitor();
 
     @Override
@@ -24,9 +24,13 @@ public class TypesValidator extends AbstractRamlValidator {
             diagnostics.add(diagnostic);
             return false;
         } else {
-            final List<Diagnostic> examplesDiagnostics = examplesValidatingVisitor.doSwitch(eObject);
-            examplesDiagnostics.forEach(diagnostics::add);
-            return examplesDiagnostics.isEmpty();
+            final List<Diagnostic> validationResults = new ArrayList<>();
+            validationResults.addAll(enumValidatingVisitor.doSwitch(eObject));
+            if (validationResults.isEmpty()) {
+                validationResults.addAll(examplesValidatingVisitor.doSwitch(eObject));
+            }
+            validationResults.forEach(diagnostics::add);
+            return validationResults.isEmpty();
         }
     }
 
@@ -96,6 +100,55 @@ public class TypesValidator extends AbstractRamlValidator {
             final List<Diagnostic> validationResults = anyType.getExamples().stream()
                     .flatMap(example -> instanceValidator.validate(example.getValue(), anyType).stream())
                     .collect(Collectors.toList());
+            return validationResults;
+        }
+    }
+
+    private class EnumValidatingVisitor extends TypesSwitch<List<Diagnostic>> {
+        private InstanceValidator instanceValidator = new InstanceValidator();
+
+        @Override
+        public List<Diagnostic> defaultCase(EObject object) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public List<Diagnostic> caseAnyType(final AnyType anyType) {
+            final List<Diagnostic> validationResults = new ArrayList<>();
+            validationResults.addAll(anyType.getEnum().stream()
+                    .flatMap(value -> instanceValidator.validate(value, anyType).stream())
+                    .collect(Collectors.toList()));
+
+            if (validationResults.isEmpty()) {
+                final Set<Object> uniqueItems = new HashSet<>();
+                // TODO this only works for primitive values, we should extend it for object instance and array instance
+                final Set<Instance> duplicateValues = anyType.getEnum().stream()
+                        .filter(value -> !uniqueItems.add(value.getValue()))
+                        .collect(Collectors.toSet());
+                if (duplicateValues.size() > 0) {
+                    validationResults.add(error("Enum facet contains duplicate values", anyType));
+                }
+            }
+            return validationResults;
+        }
+
+        @Override
+        public List<Diagnostic> caseAnyAnnotationType(final AnyAnnotationType anyAnnotationType) {
+            final List<Diagnostic> validationResults = new ArrayList<>();
+            validationResults.addAll(anyAnnotationType.getEnum().stream()
+                    .flatMap(value -> instanceValidator.validate(value, anyAnnotationType).stream())
+                    .collect(Collectors.toList()));
+
+            if (validationResults.isEmpty()) {
+                final Set<Object> uniqueItems = new HashSet<>();
+                // TODO this only works for primitive values, we should extend it for object instance and array instance
+                final Set<Instance> duplicateValues = anyAnnotationType.getEnum().stream()
+                        .filter(value -> !uniqueItems.add(value.getValue()))
+                        .collect(Collectors.toSet());
+                if (duplicateValues.size() > 0) {
+                    validationResults.add(error("Enum facet contains duplicate values", anyAnnotationType));
+                }
+            }
             return validationResults;
         }
     }
--- raml-model/src/test/groovy/io/vrap/rmf/raml/validation/StringTypeValidationTest.groovy
+++ raml-model/src/test/groovy/io/vrap/rmf/raml/validation/StringTypeValidationTest.groovy
@@ -1,5 +1,9 @@
 package io.vrap.rmf.raml.validation
 
+import io.vrap.rmf.raml.model.facets.FacetsFactory
+import io.vrap.rmf.raml.model.facets.Instance
+import io.vrap.rmf.raml.model.facets.IntegerInstance
+import io.vrap.rmf.raml.model.facets.StringInstance
 import io.vrap.rmf.raml.model.types.StringType
 import io.vrap.rmf.raml.model.types.TypesFactory
 
@@ -24,5 +28,34 @@ class StringTypeValidationTest extends BaseValidatorTest {
         -1        | null      || false
         null      | -1        || false
         1         | 2         || true
+    }
+
+    def "validateEnumFacet"() {
+        when:
+        enumValues.each {
+            Instance instance
+            switch (it) {
+                case Integer:
+                    IntegerInstance integerInstance = FacetsFactory.eINSTANCE.createIntegerInstance()
+                    integerInstance.value = it
+                    instance = integerInstance
+                    break
+                case String:
+                    StringInstance stringInstance = FacetsFactory.eINSTANCE.createStringInstance()
+                    stringInstance.value = it
+                    instance = stringInstance
+                    break
+                default:
+                    true == false
+            }
+            stringType.enum.add(instance)
+        }
+        then:
+        validate(stringType) == valid
+        where:
+        enumValues   || valid
+        [ 0, '1']    || false
+        [ '0', '1']  || true
+        [ '1', '1']  || false
     }
 }
