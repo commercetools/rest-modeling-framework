7a1ed0894e00bd1e12b3761337211e1091e5c0b5
Jens Schulze <jens.schulze@commercetools.de>
2017-12-01 09:35:40 +0100

    add generated hint to post man template

--- raml-generic-generator/src/main/java/io/vrap/rmf/raml/generic/generator/postman/CollectionGenerator.java
+++ raml-generic-generator/src/main/java/io/vrap/rmf/raml/generic/generator/postman/CollectionGenerator.java
@@ -57,7 +57,7 @@ public class CollectionGenerator extends AbstractTemplateGenerator {
             st.add("id", "f367b534-c9ea-e7c5-1f46-7a27dc6a30ba");
         }
         if (fileName.equals("template.json.stg"))  {
-            st.add("id", "c5566d48-02e5-742c-ff0a-a3b948c46502");
+            st.add("id", "d7d6443c-6f61-4b12-972e-5fd97532b432");
         }
         return st.render();
     }
