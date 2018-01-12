86fd32649914397baa495192da272e9cb07b943b
Jens Schulze <jens.schulze@commercetools.de>
2018-01-12 14:12:03 +0100

    remove build container use gradle cache in build container

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Building artifact:"
-docker run -v ${PWD}:/rmf -w /rmf openjdk:8-alpine ./gradlew clean build shadowJar
+docker run --rm -v ${PWD}:/rmf -v${HOME}/.gradle:/root/.gradle -w /rmf openjdk:8-alpine ./gradlew clean build shadowJar
 echo "Done."
 
 echo "Build runtime container:"
-docker build -t vrapio/rmf-generator .
+docker build --rm -t vrapio/rmf-generator .
 echo "Done."
