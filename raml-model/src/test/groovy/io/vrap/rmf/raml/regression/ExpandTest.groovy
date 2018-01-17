7729680a13df4d3135e3bf13c1625433b8097b55
Jens Schulze <jens.schulze@commercetools.de>
2018-01-17 17:30:11 +0100

    add more tests for trait expansion

--- raml-model/src/test/groovy/io/vrap/rmf/raml/regression/ExpandTest.groovy
+++ raml-model/src/test/groovy/io/vrap/rmf/raml/regression/ExpandTest.groovy
@@ -5,6 +5,8 @@ import io.vrap.rmf.raml.model.modules.Api
 import spock.lang.Ignore
 
 class ExpandTest extends RegressionTest {
+
+    @Ignore
     def "expand-traits-with-resource-type" () {
         when:
         RamlModelResult<Api> ramlModelResult = constructApi(
@@ -16,6 +18,8 @@ class ExpandTest extends RegressionTest {
                 queryParameters:
                     version:
                         type: number
+                        minimum: 10
+                        maximum: 20
         resourceTypes:
             base:
                 delete:
@@ -23,10 +27,17 @@ class ExpandTest extends RegressionTest {
                         - versioned
         /category:
             type: base
+            delete:
+                queryParameters:
+                    version:
+                        type: number 
+                        minimum: 3
         ''')
         then:
         ramlModelResult.validationResults.size() == 0
         ramlModelResult.rootObject.resources.get(0).methods.get(0).queryParameters.size() == 1
+        ramlModelResult.rootObject.resources.get(0).methods.get(0).queryParameters[0].type.minimum == 3
+        ramlModelResult.rootObject.resources.get(0).methods.get(0).queryParameters[0].type.maximum == 20
     }
 
     @Ignore
@@ -49,5 +60,37 @@ class ExpandTest extends RegressionTest {
         then:
         ramlModelResult.validationResults.size() == 0
         ramlModelResult.rootObject.resources.get(0).methods.get(0).queryParameters.size() == 1
+    }
+
+    @Ignore
+    def "expand-traits-with-resource-type-in-resource" () {
+        when:
+        RamlModelResult<Api> ramlModelResult = constructApi(
+                '''\
+        #%RAML 1.0
+        title: Some API
+        traits:
+            versioned:
+                queryParameters:
+                    version:
+                        type: number
+            stageable:
+                queryParameters:
+                    staged:
+                        type: boolean
+        resourceTypes:
+            base:
+                delete:
+                    is:
+                        - stageable
+        /category:
+            type: base
+            delete:
+                is:
+                    - versioned
+        ''')
+        then:
+        ramlModelResult.validationResults.size() == 0
+        ramlModelResult.rootObject.resources.get(0).methods.get(0).queryParameters.size() == 2
     }
 }
