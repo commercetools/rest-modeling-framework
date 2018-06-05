9e011965489fcb03eecb5bffcf24fb4cab9ebd25
mkoester <matthias.koester@commercetools.de>
2018-06-05 18:15:59 +0200

    Add test that Jens reported on the raml java parser project.

--- raml-model/src/test/groovy/io/vrap/rmf/raml/regression/ExpandTest.groovy
+++ raml-model/src/test/groovy/io/vrap/rmf/raml/regression/ExpandTest.groovy
@@ -3,6 +3,10 @@ package io.vrap.rmf.raml.regression
 import io.vrap.rmf.raml.model.RamlModelResult
 import io.vrap.rmf.raml.model.modules.Api
 import io.vrap.rmf.raml.model.resources.HttpMethod
+import io.vrap.rmf.raml.model.resources.Method
+import io.vrap.rmf.raml.model.resources.Resource
+import io.vrap.rmf.raml.model.responses.Response
+import io.vrap.rmf.raml.model.types.AnyType
 
 class ExpandTest extends RegressionTest {
 
@@ -193,5 +197,51 @@ class ExpandTest extends RegressionTest {
         with(ramlModelResult.rootObject.resources[0]) {
             methods.size() == 2
         }
+    }
+
+    def "ResourceType merge issue"() {
+        when:
+        RamlModelResult<Api> ramlModelResult = constructApi(
+                '''\
+        #%RAML 1.0
+        ---
+        title: Test
+        baseUri: https://example.com
+        version: v1
+        mediaType: application/json
+        types:
+            PagedQueryResponse: object
+        resourceTypes:
+            domainTemplate:
+                get:
+                    responses:
+                        200:
+                            body:
+                                type: <<templateType>>
+        /categories:
+            type: { domainTemplate: { templateType: PagedQueryResponse } }
+            displayName: Categories
+            description: Categories are used to organize products in a hierarchical structure.
+            get:
+                description: Query Categories
+                responses:
+                200:
+                    body:
+                        application/json:
+                            example: {}        
+        ''')
+        then:
+        ramlModelResult.validationResults.size() == 0
+        Api api = ramlModelResult.rootObject
+        AnyType pagedQueryResponse = api.getType('PagedQueryResponse')
+        pagedQueryResponse != null
+        api.resources.size() == 1
+        Resource categoriesResource = api.resources[0]
+        Method getMethod = categoriesResource.getMethod(HttpMethod.GET)
+        getMethod != null
+        getMethod.responses.size() == 1
+        Response response = getMethod.responses[0]
+        response.bodies.size() == 1
+        response.bodies[0].type == pagedQueryResponse
     }
 }
