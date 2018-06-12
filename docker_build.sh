23ed471c8eb47de541ca06f9d74cd3888b698df3
Jens Schulze <jenschude@gmail.com>
2018-06-12 15:29:48 +0200

    refactor build script for rmf generator

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Building artifact:"
-docker run --rm -v ${PWD}:/rmf -v${HOME}/.gradle:/root/.gradle -w /rmf openjdk:8-alpine ./gradlew clean build shadowJar
-echo "Done."
-
 echo "Build runtime container:"
 docker build --rm -t vrapio/rmf-generator .
 echo "Done."
