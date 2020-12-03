84682aeec3e5d9f197e4a5fc83273c80f07f3d39
Jens Schulze <jenschude@gmail.com>
2020-12-03 10:40:54 +0100

    add regression test for query parameter merge

--- raml-model/src/test/groovy/io/vrap/rmf/raml/regression/TraitTest.groovy
+++ raml-model/src/test/groovy/io/vrap/rmf/raml/regression/TraitTest.groovy
@@ -5,6 +5,7 @@ import io.vrap.rmf.raml.model.modules.Api
 import io.vrap.rmf.raml.model.resources.HttpMethod
 import io.vrap.rmf.raml.model.resources.Method
 import io.vrap.rmf.raml.model.resources.Resource
+import spock.lang.Ignore
 
 class TraitTest extends RegressionTest {
 
@@ -41,5 +42,56 @@ class TraitTest extends RegressionTest {
         method != null
         method.responses.size() == 2
         method.responses.find {it.statusCode == "201"}.description.value == 'Post created, returns the created post.'
+    }
+
+    @Ignore
+    def "queryparameter"() {
+        when:
+        RamlModelResult<Api> ramlModelResult = constructApi(
+                '''\
+                #%RAML 1.0
+                title: Example API
+                version: v1
+                baseUri: https://api.example.org
+                traits:
+                  filter:
+                    queryParameters:
+                      date[from]?:
+                        type: string
+                      from?:
+                        type: string
+                      date_from:
+                        type: string
+                      dates_from:
+                        type: string
+                        required: false
+                resourceTypes:
+                  test:
+                    get:
+                      is:
+                        - filter
+                /servers:
+                  type: test
+                  get:
+                    queryParameters:
+                      date[to]?:
+                        type: string
+                      to?:
+                        type: string
+                      date_to:
+                        type: string
+                      dates_to:
+                        type: string
+                        required: false
+                    responses:
+                        200:
+        ''')
+        then:
+        ramlModelResult.validationResults.size() == 0
+        ramlModelResult.rootObject.resources.size() == 1
+        Resource resource = ramlModelResult.rootObject.resources[0]
+        Method method = resource.getMethod(HttpMethod.GET)
+        method != null
+        method.queryParameters.size() == 8
     }
 }
