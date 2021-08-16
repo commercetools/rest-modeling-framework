7ef9e35efabc9bf0e08dc0b8f453be2beabd0a6f
Jens Schulze <jenschude@gmail.com>
2021-08-16 15:10:53 +0200

    add test for custom validator

---
+++ raml-model/src/test/groovy/io/vrap/rmf/raml/regression/CustomValidatorTest.groovy
@@ -0,0 +1,50 @@
+package io.vrap.rmf.raml.regression
+
+import io.vrap.rmf.raml.model.RamlModelResult
+import io.vrap.rmf.raml.model.modules.Api
+import io.vrap.rmf.raml.model.modules.util.ModulesSwitch
+import io.vrap.rmf.raml.validation.AbstractRamlValidator
+import io.vrap.rmf.raml.validation.RamlValidator
+import org.eclipse.emf.common.util.Diagnostic
+import org.eclipse.emf.common.util.DiagnosticChain
+import org.eclipse.emf.ecore.EClass
+import org.eclipse.emf.ecore.EObject
+
+class CustomValidatorTest extends RegressionTest {
+    class MyApiValidator extends AbstractRamlValidator implements RamlValidator {
+        class ApiSwitch extends ModulesSwitch<List<Diagnostic>> {
+            @Override
+            List<Diagnostic> caseApi(Api object) {
+                final List<Diagnostic> validationErrors = new ArrayList<>();
+
+                validationErrors.add(error(object, "invalid: {0}", object.title))
+
+                return validationErrors;
+            }
+        }
+        private final ModulesSwitch<List<Diagnostic>> validator = new ApiSwitch()
+        @Override
+        boolean validate(EClass eClass, EObject eObject, DiagnosticChain diagnostics, Map<Object, Object> context) {
+            final List<Diagnostic> validationErrors = new ArrayList<>();
+
+            validationErrors.addAll(validator.doSwitch(eObject));
+            validationErrors.forEach(diagnostics.&add);
+
+            return validationErrors.isEmpty();
+
+        }
+    }
+
+    def "custom-validation"() {
+        when:
+        RamlModelResult<Api> ramlModelResult = constructApi(
+                '''
+        #%RAML 1.0
+        title: Some API
+                ''', Arrays.asList(new MyApiValidator())
+        )
+        then:
+        ramlModelResult.validationResults.size() == 1
+        ramlModelResult.validationResults[0].message == "invalid: Some API"
+    }
+}
